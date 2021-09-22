6149c3c410679adaaffd276506d27a51f28cbdd9
torvicvasil <46871977+torvicvasil@users.noreply.github.com>
2021-09-21 23:26:50 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,9 @@
 from flask import Flask
+application = Flask(__name__)
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-  return "Hello World!"
-
-
-@app.route('/super_simple')
-def super_simple():
-  return "Hello from the Planetary API."
-
+@application.route("/")
+def hello():
+    return "Hello World! Teste"
 
 if __name__ == "__main__":
-  app.run()
+    application.run()
