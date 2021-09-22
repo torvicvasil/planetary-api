e82296bf72e0e52b36b43dbd20c6821178bcdbec
torvicvasil <46871977+torvicvasil@users.noreply.github.com>
2021-09-21 23:34:42 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,9 @@ application = Flask(__name__)
 def hello():
     return "Hello World! Teste"
 
+@application.route("/super_simple")
+def super_simple():
+    return "Hello from the Planetary API."
+
 if __name__ == "__main__":
     application.run()
